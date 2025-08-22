6cfae051935589949fe291b500ff29d41e69eaff
ornellasj <ornellasj@weg.net>
2025-08-21 22:59:04 -0300

    fix: configure URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dennisklappe.github.io',
+  site: "https://jrackerblog.web.app",
   // Only use base path in production (GitHub Pages)
-  base: process.env.NODE_ENV === 'production' ? '/astro-theme-terminal' : '/',
+  base: process.env.NODE_ENV === "production" ? "/astro-theme-terminal" : "/",
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'css-variables',
+      theme: "css-variables",
       langs: [],
       wrap: true,
     },
